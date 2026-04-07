3f5c631c90c824731926fa3f1b43b7ed51ee6dfd
Demilade <dolaniyan32@gmail.com>
2026-04-07 11:19:30 +0100

    Started a mini node.js project

---
+++ app.js
@@ -0,0 +1,82 @@
+console.log('Hello World!');
+
+const fs = require('fs');
+fs.readFile('myfile.txt', 'utf-8', (err, data) => {
+    if (err) {
+        console.error('Error reading file: ' + err);
+        return;
+    }
+    console.log('File contents: ' + data);
+});
+
+console.log('Reading file....(this runs first!)');
+
+
+let name = 'Node.js';
+const version = 20 ;
+function greet (user) {
+    return `Hello ${user}!`;
+}
+
+const add = (a, b) => a + b;
+console.log(greet('Developer'));
+console.log(add(5, 3));
+
+// object
+const user = {
+    name : 'Alice',
+    age : 25,
+    greet() {
+        console.log (`Hi, I'm ${this.name}`);
+    }
+};
+
+// array
+const colors = ['red', 'green', 'blue'];
+
+// array methods
+colors.forEach(color => console.log(color));
+const lengths = colors.map(color => color.length);
+
+// callbacks
+function fetchData(callback) {
+    setTimeout(() => {
+        callback('Data recieved!');
+    }, 1000);
+}
+
+// promises (ES6+)
+const fetchDataPromise = () => {
+    return new Promise((resolve) => {
+        setTimeout(() => resolve('Promise resolved!'), 1000);
+    });
+};
+
+// Async/await (ES8+)
+async function getData() {
+    const result = await fetchDataPromise();
+    console.log(result);
+}
+
+// call async function
+getData(); 
+
+// global objects
+global.mylet = 42;
+console.log(global.mylet);
+
+// file access
+// Node.js
+// const fs = require('fs');
+fs.readFile('myfile.txt', 'utf-8', (err, data) => {
+    if (err) throw err;
+    console.log(data);
+});
+
+// HTTPs Request
+const https = require('https');
+https.get('https://example.com', res =>{
+    let data = '';
+    res.on('data', chunck => data += chunck);
+    res.on('end ', () => console.log(data));
+});
